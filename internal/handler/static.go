7c6e2a28c16d358eb862798df23754052034caae
Amirabbas Fazelinia <utilyre@proton.me>
2023-09-17 15:10:09 +0330

    signup page

--- internal/handler/static.go
+++ internal/handler/static.go
@@ -17,8 +17,15 @@ func HandleStatic(r router.Router, t *template.Template) {
 		tmpl: t,
 	}
 
+	r.Handle("/signup", r.ErrorHandler.HandleFunc(h.signup)).
+		Methods(http.MethodGet)
+
 	r.Handle("/login", r.ErrorHandler.HandleFunc(h.login)).
 		Methods(http.MethodGet)
+}
+
+func (h staticHandler) signup(w http.ResponseWriter, r *http.Request) error {
+	return xmate.WriteHTML(w, h.tmpl, http.StatusOK, "signup", nil)
 }
 
 func (h staticHandler) login(w http.ResponseWriter, r *http.Request) error {
